ba1d482dab2729f74995e6bda9d2af091fc947a4
EMPAT94 <priteshtupe@gmail.com>
2015-10-08 20:18:45 +0530

    Modified Sync.bat

--- sync.bat
+++ sync.bat
@@ -2,7 +2,10 @@
 git status
 SET /P c=Continue to sync? (y/n) : 
 IF "%c%"=="y" (GOTO Add)
+IF "%c%"=="Y" (GOTO Add)
+IF "%c%"=="N" (GOTO END)
 IF "%c%"=="n" (GOTO End) ELSE (GOTO Error)
+
 :Add
 git add .
 @echo Added
@@ -12,12 +15,19 @@ git commit -m "%msg%"
 @echo Commited
 git status
 SET /P c=Push? (Y/N) : 
-IF "%c%"=="y" (git push origin master
-@echo "Sync Successful")
+IF "%c%"=="y" (GOTO Push)
+IF "%c%"=="Y" (GOTO Push)
+IF "%c%"=="N" (GOTO End)
 IF "%c%"=="n" (GOTO End) ELSE (GOTO Error)
-GOTO End
+
+:Push
+git push origin master
+@echo "Sync Successful" 
+GOTO END
+
 :Error
 ECHO Invalid Input, Exiting.
 GOTO End
+
 :End
 pause
