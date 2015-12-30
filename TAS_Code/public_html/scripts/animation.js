68aaa8b7374df52b1c5222cfdc42f77e594d1dac
Topsy Krett <priteshtupe@gmail.com>
2015-12-30 14:23:24 +0530

    Initiated UI
    Created Basic Framework for Navigation Bar, Forms, Cards and other UI
    elements.

---
+++ TAS_Code/public_html/scripts/animation.js
@@ -0,0 +1,18 @@
+
+$(document).ready(function () {
+    $('.login-btn').on('click',function () {
+        $('.login-form').slideToggle('slow');
+    });
+    
+    $('.card-action').on('click', 'a', function () {
+        $('.login').slideToggle();
+        $('.register').slideToggle();
+        var l = $('#logreg').text();
+        if(l == "Login") {
+            $('#logreg').text("Register");
+        } 
+        else {
+            $('#logreg').text("Login");
+        }
+    });
+});
