b2c73e2d19516667f63d4133b63bee657591cafc
Topsy Krett <priteshtupe@gmail.com>
2016-01-07 12:17:39 +0530

    Main Page
    Created a Main page with placeholder animations.
    Included the new Brain-centric design approach.

--- TAS_Code/public_html/scripts/animation.js
+++ TAS_Code/public_html/scripts/animation.js
@@ -1,24 +1,29 @@
 
 $(document).ready(function () {
-    
-    
-    $('.login-btn').on('click',function () {
+    $('.welcome').delay('slow').fadeIn('slow');
+    $('.welcome').delay('slow').fadeOut('slow');
+    $('.rest').delay(2500).slideDown(1000);
+    $('#brain').click(function () {
+        $(this).closest('div').delay('fast').fadeOut();
+        $(this).closest('.mainBrain').find('.brainParts').delay('slow').fadeIn('slow');
+    });
+
+    $('.login-btn').on('click', function () {
         $('.login-form').slideToggle('slow');
     });
-    
+
     $('.card-action').on('click', 'a', function () {
         $('.login').slideToggle();
         $('.register').slideToggle();
         var l = $('#logreg').text();
-        if(l == "Login") {
+        if (l == "Login") {
             $('#logreg').text("Register");
-        } 
-        else {
+        } else {
             $('#logreg').text("Login");
         }
     });
-    
-    $('.menu_white').on('click',function () {
+
+    $('.menu_white').on('click', function () {
         $(this).closest('div').find('.button-collapse').sideNav();
     });
 });
