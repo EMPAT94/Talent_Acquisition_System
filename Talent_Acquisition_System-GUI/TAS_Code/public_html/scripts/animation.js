1b176737dea78cd0c9e6d17189937c6aeb560084
deepak_420 <deepakmmali8898@gmaiil.com>
2016-01-08 19:21:03 +0530

    Front end
    Front end is working.  Login page is to be linked. and images on some
    pages need to be replaced as they content watermark of respective
    websires.

---
+++ Talent_Acquisition_System-GUI/TAS_Code/public_html/scripts/animation.js
@@ -0,0 +1,29 @@
+
+$(document).ready(function () {
+    $('.welcome').delay('slow').fadeIn('slow');
+    $('.welcome').delay('slow').fadeOut('slow');
+    $('.rest').delay(2500).slideDown(1000);
+    $('.brain').click(function () {
+        $(this).closest('div').delay('fast').fadeOut();
+        $(this).closest('.mainBrain').find('.brainParts').delay('slow').fadeIn('slow');
+    });
+
+    $('.login-btn').on('click', function () {
+        $('.login-form').slideToggle('slow');
+    });
+
+    $('.card-action').on('click', 'a', function () {
+        $('.login').slideToggle();
+        $('.register').slideToggle();
+        var l = $('#logreg').text();
+        if (l == "Login") {
+            $('#logreg').text("Register");
+        } else {
+            $('#logreg').text("Login");
+        }
+    });
+
+    $('.menu_white').on('click', function () {
+        $(this).closest('div').find('.button-collapse').sideNav();
+    });
+});
